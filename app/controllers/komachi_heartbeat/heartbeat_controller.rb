49730e71af45fecb561d1a765098a0d97a36ee89
Takumi MIURA <mitaku1104@gmail.com>
2014-01-11 18:11:39 +0900

    heartbeatで正常な場合bodyに'ok'を入れて返すように修正

--- app/controllers/komachi_heartbeat/heartbeat_controller.rb
+++ app/controllers/komachi_heartbeat/heartbeat_controller.rb
@@ -9,7 +9,7 @@ module KomachiHeartbeat
         redis_connection_check if redis_check?
         memcached_connection_check if memcached_check?
 
-        head :ok
+        render text: "ok", status: 200
       rescue => e
         head :internal_server_error
       end
