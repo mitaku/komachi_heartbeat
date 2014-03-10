33f0fc373c85bdcb83960ce936bfca8a0792fcb8
gussan <egussan@gmail.com>
2014-03-10 16:03:59 +0900

    Add feature: worker stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 KomachiHeartbeat::Engine.routes.draw do
   get "heartbeat", :to => "heartbeat#index"
   get "version", :to => "heartbeat#version"
+
+  get "stats/worker", :to => "stats#worker"
 end
--- lib/komachi_heartbeat/config.rb
+++ lib/komachi_heartbeat/config.rb
@@ -1,7 +1,7 @@
 module KomachiHeartbeat
   class Config
     include ActiveSupport::Configurable
-    config_accessor :application_version, :application_name, :db_check_enabled, :redis_check_enabled, :memcached_check_enabled, :database_class_names, :memcached_server_names, :redis_servers
+    config_accessor :application_version, :application_name, :db_check_enabled, :redis_check_enabled, :memcached_check_enabled, :database_class_names, :memcached_server_names, :redis_servers, :worker_stats_enabled
 
     configure do |config|
       config.application_name = "Unknown"
@@ -14,6 +14,8 @@ module KomachiHeartbeat
 
       config.memcached_check_enabled = false
       config.memcached_server_names = ["localhost"]
+
+      config.worker_stats_enabled = false
     end
   end
 end
