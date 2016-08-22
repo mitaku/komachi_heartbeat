4dfd603faaa67b5c2140b0bb553906a211948ce0
sue445 <sue445@sue445.net>
2016-08-22 22:07:21 +0900

    Fix NameError on ruby 1.9
    `__dir__` requiments ruby 2.0+

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir["#{__dir__}/support/**/*.rb"].each {|f| require f}
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
   RSpec.configure do |config|
     # ## Mock Framework
